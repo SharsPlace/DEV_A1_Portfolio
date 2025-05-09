7e87f9bde396e930124a142450aac379b9ed9e1a
Shar Dunn <14262@coderacademy.edu.au>
2025-05-10 02:50:09 +1000

    try using php for header and footer

---
+++ src/Header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Portfolio</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header>
+    <h1>Welcome to My Website</h1>
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="contact.php">Contact</a>
+    </nav>
+  </header>
